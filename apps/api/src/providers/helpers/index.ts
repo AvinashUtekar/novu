647b66b480ff054dfc33e319462f581526c9df38
p-fernandez <pablo.fernandez.otero@gmail.com>
2023-03-08 18:04:51 +0000

    feat(api): sendgrid regression test suite

--- apps/api/src/providers/helpers/index.ts
+++ apps/api/src/providers/helpers/index.ts
@@ -1,2 +1,4 @@
+export * from './constants';
+export { createSubscriber, findMessageIdByTransactionId, triggerEvent } from './events';
 export { checkProviderIntegration, createProviderIntegration } from './integrations';
 export { createRegressionNotificationTemplate } from './notification-templates';
